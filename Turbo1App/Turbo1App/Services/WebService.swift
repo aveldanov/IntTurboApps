edab45673b7d2ba5679b724224ea714cd1d040bc
Anton Veldanov <anton_veldanov@intuit.com>
2021-04-22 22:21:04 -0700

    iOS

---
+++ Turbo1App/Turbo1App/Services/WebService.swift
@@ -0,0 +1,26 @@
+//
+//  WebService.swift
+//  Turbo1App
+//
+//  Created by Veldanov, Anton on 4/22/21.
+//
+
+import Foundation
+
+
+class WebService{
+    
+    
+    func fetchData(url:URL, completetion:@escaping (Result<Any,Error>)->()){
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            print(data)
+            
+            
+            
+        }.resume()
+        
+        
+    }
+    
+    
+}
