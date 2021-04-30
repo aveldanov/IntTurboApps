6ab243cc192e4ef6d21faada0d123464e9839838
Anton Veldanov <anton.veldanov@gmail.com>
2021-04-29 22:39:24 -0700

    iOS

--- PlanYourPlanApp/PlanYourPlanApp/PlanListViewController.swift
+++ PlanYourPlanApp/PlanYourPlanApp/PlanListViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class PlanListViewController: UITableViewController {
 
+    let itemArray = ["A","B","C"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
       
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "planItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        
+        return cell
+    }
+// TableView Delegate Methods
+    
+    
 
-
+    
 }
 
