d52b8f631c8da2fcf39e0e52f172bb397fd1fbce
Anton Veldanov <anton.veldanov@gmail.com>
2021-04-29 22:28:26 -0700

    iOS

---
+++ PlanYourPlanApp/PlanYourPlanApp/PlanListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlanYourPlanApp
+//
+//  Created by Anton Veldanov on 4/29/21.
+//
+
+import UIKit
+
+class PlanListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+    }
+
+
+}
+
