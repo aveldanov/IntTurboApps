f5232059e225c0f2b3926759d84e36e1876fc385
Anton Veldanov <anton.veldanov@gmail.com>
2021-05-13 16:09:32 -0700

    iOS

--- Practice5App/Practice5App/Controllers/DetailViewController.swift
+++ Practice5App/Practice5App/Controllers/DetailViewController.swift
@@ -9,13 +9,18 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
-    
+    var nameLoaded: String?
+    var keyLoaded: String?
     @IBOutlet weak var detailNameLabel: UILabel!
+    @IBOutlet weak var detailKeyLabel: UILabel!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        detailNameLabel.text = nameLoaded
+        
+        detailKeyLabel.text = keyLoaded
         // Do any additional setup after loading the view.
     }
     
