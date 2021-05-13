5fd36dd8decfc1042b818a42d54056b665e1d9ba
Anton Veldanov <anton.veldanov@gmail.com>
2021-05-13 15:39:50 -0700

    iOS

---
+++ Practice5App/Practice5App/Services/WebService.swift
@@ -0,0 +1,25 @@
+//
+//  WebService.swift
+//  Practice5App
+//
+//  Created by Veldanov, Anton on 5/13/21.
+//
+
+import Foundation
+
+
+
+class WebService{
+    
+    
+    
+    func fetchData(_ url:URL, completion: (Result<[Any],Error>)->()){
+        
+        
+        
+        
+    }
+    
+    
+    
+}
