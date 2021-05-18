85526efb417d7f0987c004baad159ae5c425cd51
Anton Veldanov <anton.veldanov@gmail.com>
2021-05-17 20:30:05 -0700

    iOS

--- MultiThredingPG.playground/Contents.swift
+++ MultiThredingPG.playground/Contents.swift
@@ -1,1 +1,26 @@
+import UIKit
 
+
+
+//DispatchQueue.global().sync {
+//    sleep(2)
+//    print("sync: inside")
+//}
+//
+//print("sync: outside")
+//DispatchQueue.global().async {
+//    sleep(2)
+//    print("sync: inside")
+//}
+//
+//print("sync: outside")
+
+
+DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + .seconds(2)) {
+    print("First delay 2 seconds")
+}
+
+
+DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+    print("Second delat - 2second")
+}
