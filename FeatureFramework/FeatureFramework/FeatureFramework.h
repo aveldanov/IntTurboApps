ac723c7a00f30b569d08e509a9d2edf9c8dc8e6a
Anton Veldanov <anton.veldanov@gmail.com>
2021-05-19 09:28:42 -0700

    iOS

---
+++ FeatureFramework/FeatureFramework/FeatureFramework.h
@@ -0,0 +1,18 @@
+//
+//  FeatureFramework.h
+//  FeatureFramework
+//
+//  Created by Anton Veldanov on 5/19/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FeatureFramework.
+FOUNDATION_EXPORT double FeatureFrameworkVersionNumber;
+
+//! Project version string for FeatureFramework.
+FOUNDATION_EXPORT const unsigned char FeatureFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FeatureFramework/PublicHeader.h>
+
+
---
+++ FeatureFramework/FeatureFramework/MyViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MyViewController.swift
+//  MyFramework
+//
+//  Created by Nazario Mariano on 1/20/20.
+//  Copyright © 2020 Nazario Mariano. All rights reserved.
+//
+
+import UIKit
+
+class MyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
