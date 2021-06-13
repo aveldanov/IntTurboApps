7bde00e4d7297eb9d3dbbefb06b39b8c323f7dfe
Anton Veldanov <anton.veldanov@gmail.com>
2021-06-13 14:12:34 -0700

    iOS

---
+++ InAppPurchaseFeatureApp/InAppPurchaseFeatureApp/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  InAppPurchaseFeatureApp
+//
+//  Created by Anton Veldanov on 6/13/21.
+//
+
+import UIKit
+import StoreKit
+
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+ 
+    }
+    
+    
+    @IBAction func buyBtnClicked(_ sender: UIButton) {
+        
+        
+    }
+    
+
+
+}
+
